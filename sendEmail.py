013dfeeb0b43a4a50c4a8b6eca0f08011e3f5970
shivapbhusal <civabhusal@gmail.com>
2018-03-06 16:44:59 -0500

    sendEmail

--- sendEmail.py
+++ sendEmail.py
@@ -1,29 +1,32 @@
 '''
-Python Script to send update emails. 
-
+Python Script to check the download status and periodically send status via email. 
 Created By: Shiva Bhusal, BGSU 
 '''
 import smtplib
+import time
 
-gmail_user = 'civabhusal@gmail.com'  
-gmail_password = '$password'
+gmail_user = 'appdownload.updates@gmail.com'  
+gmail_password ='$Password!'
 
 sent_from = gmail_user  
-to = ['sbhusal@bgsu.edu']  
+to = ['nadeez.civa@gmail.com']  
 subject = 'Message'  
 body = 'Hey, what'
 
 email_text = 'abcd'
 
-try:  
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_password)
-    server.sendmail(sent_from, to, email_text)
-    server.close()
+while (True):
+	try:  
+		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+		server.ehlo()
+		server.login(gmail_user, gmail_password)
+		server.sendmail(sent_from, to, email_text)
+		server.close()
+		print 'Email sent!'
+	
+	except:  
+		print 'Something went wrong...'
 
-    print 'Email sent!'
-except:  
-    print 'Something went wrong...'
+	time.sleep(60)
 
 
